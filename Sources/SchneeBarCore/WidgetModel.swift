8c7eb57c8d24cd5896203f81f4642ec9b08ce170
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-12 23:35:01 +0900

    feat: add default widget ordering

--- Sources/SchneeBarCore/WidgetModel.swift
+++ Sources/SchneeBarCore/WidgetModel.swift
@@ -121,6 +121,7 @@ public enum WidgetRefreshPolicy: Equatable, Sendable {
 public struct WidgetDescriptor: Equatable, Sendable {
     public let id: WidgetID
     public let displayName: String
+    public let defaultOrder: Int
     public let defaultRepresentation: WidgetRepresentationKind
     public let visibilityPolicy: WidgetVisibilityPolicy
     public let refreshPolicy: WidgetRefreshPolicy
@@ -128,12 +129,14 @@ public struct WidgetDescriptor: Equatable, Sendable {
     public init(
         id: WidgetID,
         displayName: String,
+        defaultOrder: Int = 0,
         defaultRepresentation: WidgetRepresentationKind = .normal,
         visibilityPolicy: WidgetVisibilityPolicy = .always,
         refreshPolicy: WidgetRefreshPolicy = .manual
     ) {
         self.id = id
         self.displayName = displayName
+        self.defaultOrder = defaultOrder
         self.defaultRepresentation = defaultRepresentation
         self.visibilityPolicy = visibilityPolicy
         self.refreshPolicy = refreshPolicy
