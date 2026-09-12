35bb5f8a978be840782d6e01491bc253c729bcc6
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2026-09-13 00:41:43 +0900

    refactor: stabilize repository selection persistence schema

--- Sources/SchneeBarGitHub/GitHubConnectionProfile.swift
+++ Sources/SchneeBarGitHub/GitHubConnectionProfile.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum GitHubRepositoryMonitoringSelection: Codable, Equatable, Sendable {
+public enum GitHubRepositoryMonitoringSelection: Equatable, Sendable {
     case allAccessible
     case selected(Set<Int64>)
 
@@ -10,6 +10,40 @@ public enum GitHubRepositoryMonitoringSelection: Codable, Equatable, Sendable {
             return true
         case let .selected(ids):
             return ids.contains(repositoryID)
+        }
+    }
+}
+
+extension GitHubRepositoryMonitoringSelection: Codable {
+    private enum Mode: String, Codable {
+        case allAccessible
+        case selected
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case mode
+        case repositoryIDs
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        switch try container.decode(Mode.self, forKey: .mode) {
+        case .allAccessible:
+            self = .allAccessible
+        case .selected:
+            let ids = try container.decode([Int64].self, forKey: .repositoryIDs)
+            self = .selected(Set(ids))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .allAccessible:
+            try container.encode(Mode.allAccessible, forKey: .mode)
+        case let .selected(ids):
+            try container.encode(Mode.selected, forKey: .mode)
+            try container.encode(ids.sorted(), forKey: .repositoryIDs)
         }
     }
 }
